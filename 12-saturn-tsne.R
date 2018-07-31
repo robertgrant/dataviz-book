2b4778728637f780100c08258d8a8a20b99cd2f1
Robert Grant <robertstats@live.com>
2018-07-31 16:01:10 +0100

    unlicense in saturn

--- 12-saturn-tsne.R
+++ 12-saturn-tsne.R
@@ -1,8 +1,36 @@
 # saturn tsne
 
+############################################################################
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# For more information, please refer to <http://unlicense.org/>
+############################################################################
+
+
 library(dplyr)
 library(tsne)
-splot0<-function(x) {  
+splot0<-function(x) {
   plot(x,bty='n',xaxt='n',yaxt='n',xlab="",ylab="",
                            cex=0.8,pch=19,col='#00000066')
 }
@@ -32,8 +60,8 @@ splot(tsne.saturn,"saturn_tsne")
 
 ###################  additional blog images  ##################
 
-north_south_cols<-colorspace::diverge_hcl(10, 
-                                          h = c(246, 40), 
+north_south_cols<-colorspace::diverge_hcl(10,
+                                          h = c(246, 40),
                                           c = 96,
                                           l = c(65, 90))
 north_south_cats<-floor(1+(9.99*(saturn[,3]-min(saturn[,3]))/
@@ -59,8 +87,8 @@ plot(tsne.saturn[,1:2],bty='n',xaxt='n',yaxt='n',xlab="",ylab="",
 
 # try different perplexities
 tsne.saturn<-tsne(saturn,perplexity=150)
-north_south_cols<-colorspace::diverge_hcl(10, 
-                                          h = c(246, 40), 
+north_south_cols<-colorspace::diverge_hcl(10,
+                                          h = c(246, 40),
                                           c = 96,
                                           l = c(65, 90))
 north_south_cats<-floor(1+(9.99*(saturn[,3]-min(saturn[,3]))/
